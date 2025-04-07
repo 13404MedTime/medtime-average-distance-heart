a7a73e272cee7600c2e4ee3c0cd2be78dedf4830
ibrokhimov.fr <fr.ibrokhimov@gmail.com>
2025-04-07 14:06:34 +0500

    Handle Function and GetSingleObject

--- cardioapp-average-distance-heart/handler.go
+++ cardioapp-average-distance-heart/handler.go
@@ -14,87 +14,165 @@ import (
 
 // Datas This is response struct from create
 type Datas struct {
-    Data struct {
-        Data struct {
-            Data map[string]interface{} `json:"data"`
-        } `json:"data"`
-    } `json:"data"`
+	Data struct {
+		Data struct {
+			Data map[string]interface{} `json:"data"`
+		} `json:"data"`
+	} `json:"data"`
 }
 
 // ClientApiResponse This is get single api response
 type ClientApiResponse struct {
-    Data ClientApiData `json:"data"`
+	Data ClientApiData `json:"data"`
 }
 
 type ClientApiData struct {
-    Data ClientApiResp `json:"data"`
+	Data ClientApiResp `json:"data"`
 }
 
 type ClientApiResp struct {
-    Response map[string]interface{} `json:"response"`
+	Response map[string]interface{} `json:"response"`
 }
 
+type Response struct {
+	Status string                 `json:"status"`
+	Data   map[string]interface{} `json:"data"`
+}
+
+// NewRequestBody's Data (map) field will be in this structure
+//.   fields
+// objects_ids []string
+// table_slug string
+// object_data map[string]interface
+// method string
+// app_id string
+
+// but all field will be an interface, you must do type assertion
+
 type HttpRequest struct {
-    Method  string      `json:"method"`
-    Path    string      `json:"path"`
-    Headers http.Header `json:"headers"`
-    Params  url.Values  `json:"params"`
-    Body    []byte      `json:"body"`
+	Method  string      `json:"method"`
+	Path    string      `json:"path"`
+	Headers http.Header `json:"headers"`
+	Params  url.Values  `json:"params"`
+	Body    []byte      `json:"body"`
 }
 
 type AuthData struct {
-    Type string                 `json:"type"`
-    Data map[string]interface{} `json:"data"`
+	Type string                 `json:"type"`
+	Data map[string]interface{} `json:"data"`
 }
 
 type NewRequestBody struct {
-    RequestData HttpRequest            `json:"request_data"`
-    Auth        AuthData               `json:"auth"`
-    Data        map[string]interface{} `json:"data"`
+	RequestData HttpRequest            `json:"request_data"`
+	Auth        AuthData               `json:"auth"`
+	Data        map[string]interface{} `json:"data"`
 }
-
 type Request struct {
-    Data map[string]interface{} `json:"data"`
+	Data map[string]interface{} `json:"data"`
 }
 
+// GetListClientApiResponse This is get list api response
 type GetListClientApiResponse struct {
-    Data GetListClientApiData `json:"data"`
+	Data GetListClientApiData `json:"data"`
 }
 
 type GetListClientApiData struct {
-    Data GetListClientApiResp `json:"data"`
+	Data GetListClientApiResp `json:"data"`
 }
 
 type GetListClientApiResp struct {
-    Response []map[string]interface{} `json:"response"`
+	Response []map[string]interface{} `json:"response"`
 }
 
 func DoRequest(url string, method string, body interface{}, appId string) ([]byte, error) {
-    data, err := json.Marshal(&body)
-    if err != nil {
-        return nil, err
-    }
-    client := &http.Client{
-        Timeout: time.Duration(5 * time.Second),
-    }
+	data, err := json.Marshal(&body)
+	if err != nil {
+		return nil, err
+	}
+	client := &http.Client{
+		Timeout: time.Duration(5 * time.Second),
+	}
 
-    request, err := http.NewRequest(method, url, bytes.NewBuffer(data))
-    if err != nil {
-        return nil, err
-    }
-    request.Header.Add("authorization", "API-KEY")
-    request.Header.Add("X-API-KEY", appId)
+	request, err := http.NewRequest(method, url, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("authorization", "API-KEY")
+	request.Header.Add("X-API-KEY", appId)
 
-    resp, err := client.Do(request)
-    if err != nil {
-        return nil, err
-    }
-    defer resp.Body.Close()
+	resp, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-    respByte, err := io.ReadAll(resp.Body)
-    if err != nil {
-        return nil, err
-    }
+	respByte, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 
-    return respByte, nil
+	return respByte, nil
+}
+
+// Handle a serverless request
+func Handle(req []byte) string {
+	var response Response
+	var request NewRequestBody
+	const urlConst = "https://api.admin.u-code.io"
+
+	err := json.Unmarshal(req, &request)
+	if err != nil {
+		response.Data = map[string]interface{}{"message": "Error while unmarshalling request"}
+		response.Status = "error"
+		responseByte, _ := json.Marshal(response)
+		return string(responseByte)
+	}
+	if request.Data["app_id"] == nil {
+		response.Data = map[string]interface{}{"message": "App id required"}
+		response.Status = "error"
+		responseByte, _ := json.Marshal(response)
+		return string(responseByte)
+	}
+	appId := cast.ToString(request.Data["app_id"])
+	clientId := cast.ToString(request.Data["client_id"])
+
+	// you may change table slug  it's related your business logic
+	var tableSlug = "cleints"
+
+	//get single by guid response example
+	res, err, response := GetSingleObject(urlConst, tableSlug, appId, clientId)
+	if err != nil {
+		responseByte, _ := json.Marshal(response)
+		return string(responseByte)
+	}
+
+	data := map[string]interface{}{
+		"avarage_distance": cast.ToFloat64(res.Response["avarage_distance"]),
+		"avarage_heart":    cast.ToFloat64(res.Response["avarage_heart"]),
+	}
+
+	response.Data = map[string]interface{}{"data": data}
+	response.Status = "done" //if all will be ok else "error"
+	responseByte, _ := json.Marshal(response)
+
+	return string(responseByte)
+}
+
+func GetSingleObject(url, tableSlug, appId, guid string) (ClientApiResp, error, Response) {
+	response := Response{}
+
+	var getSingleObject ClientApiResp
+	getSingleResponseInByte, err := DoRequest(url+"/v1/object/"+tableSlug+"/"+guid+"?from-ofs=true", "GET", nil, appId)
+	if err != nil {
+		response.Data = map[string]interface{}{"message": "Error while getting single object"}
+		response.Status = "error"
+		return ClientApiResp{}, errors.New("error"), response
+	}
+	err = json.Unmarshal(getSingleResponseInByte, &getSingleObject)
+	if err != nil {
+		response.Data = map[string]interface{}{"message": "Error while unmarshalling single object"}
+		response.Status = "error"
+		return ClientApiResp{}, errors.New("error"), response
+	}
+	return getSingleObject, nil, response
 }
